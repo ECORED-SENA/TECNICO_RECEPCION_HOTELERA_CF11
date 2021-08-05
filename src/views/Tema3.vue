e6cf21f2b692ac25ee313f0c0b7c16a89f913336
ricardog07 <71521082+ricardog07@users.noreply.github.com>
2021-08-05 16:17:34 -0500

    Se corrige H31

--- src/views/Tema3.vue
+++ src/views/Tema3.vue
@@ -34,7 +34,7 @@
 
     figure.mb-5
       .video
-        iframe(width="560" height="315" src="https://www.youtube.com/embed/6KYDDCnCVhc" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen)
+        iframe(width="560" height="315" src="https://www.youtube.com/embed/oxzRCxlLcUo" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen)
       
 
     figure.mb-5
